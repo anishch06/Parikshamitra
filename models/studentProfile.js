c01b6d01301b4069dcf16f9ba54abd92607f6c4a
anishch06 <anish.chawla.06@gmail.com>
2024-12-15 22:55:08 +0530

    Update studentProfile.js

--- models/studentProfile.js
+++ models/studentProfile.js
@@ -6,19 +6,21 @@ const studentProfileSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
   enrolledCourses: [{ type: String }], // Array of course names or IDs
   createdAt: { type: Date, default: Date.now },
+
+  phoneNumber: { type: String },// — For contact information.
+  address: { type: String },// — Physical address of the student.
+  profilePicture: { type: String }, //— URL or path to student's profile image.
+  status: { type: String, enum: ['active', 'inactive'] } ,//— Current status of the student.
+  dateOfBirth: { type: Date } ,//— Student's date of birth
+  assignedTeacher: { type: mongoose.Schema.Types.ObjectId, ref: 'TeacherProfile' },
+  teacherFeedback: [{ type: String }],  // Feedback from teachers about the student
+  
+  fatherName: { type: String },
+  motherName: { type: String },
+  guardianName: { type: String },
+  fatherContact: { type: String },
+  motherContact: { type: String },
+  guardianContact: { type: String }, //if applicable
 });
 
 module.exports = mongoose.model('StudentProfile', studentProfileSchema);
-/*
-{
-  "name": "John Doe",
-  "age": 20,
-  "email": "john.doe@example.com",
-  "enrolledCourses": [
-    "Math101",
-    "Physics201",
-    "ComputerScience305"
-  ],
-  "createdAt": "2024-12-11T10:30:00Z"
-}
-*/
