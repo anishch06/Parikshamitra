90e87985c67db71e40e4492500b2577fd1eb0a3a
anishch06 <anish.chawla.06@gmail.com>
2024-12-15 22:58:09 +0530

    Update chapter.js

--- models/chapter.js
+++ models/chapter.js
@@ -6,6 +6,9 @@ const chapterSchema = new mongoose.Schema({
     description: { type: String },
     order: { type: Number, required: true }, // Order of the chapter in the course
     createdAt: { type: Date, default: Date.now },
+    duration: { type: Number }, //— Estimated time (in hours) to complete the chapter.
+    resources: [ { type: String } ] ,//— List of resources like books, links, or PDFs associated with the chapter.
+    isPublished: { type: Boolean, default: true },// — Whether the chapter is published.
   });
   
   module.exports = mongoose.model('chapter', chapterSchema);
