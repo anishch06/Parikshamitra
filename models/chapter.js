6b24a869ab4e737c795d49baa0895b9f58fb2907
anishch06 <anish.chawla.06@gmail.com>
2024-12-15 23:11:55 +0530

    Update chapter.js

--- models/chapter.js
+++ models/chapter.js
@@ -4,7 +4,6 @@ const chapterSchema = new mongoose.Schema({
     courseId: { type: String, required: true }, // Course ID to which the chapter belongs
     title: { type: String, required: true },
     description: { type: String },
-    order: { type: Number, required: true }, // Order of the chapter in the course
     createdAt: { type: Date, default: Date.now },
     duration: { type: Number }, //— Estimated time (in hours) to complete the chapter.
     resources: [ { type: String } ] ,//— List of resources like books, links, or PDFs associated with the chapter.
